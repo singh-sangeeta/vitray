39e730496564640d20e88b967db70649566ade2f
singh-sangeeta <sangeetagehlot98@gmail.com>
2020-09-21 00:40:42 +0600

    create model

--- products/models.py
+++ products/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 class products(models.Model):
+    productid = models.IntegerField()
     productname = models.CharField(max_length=50)
     productdetails = models.CharField(max_length=100)
     createddate = models.DateField()
     deleteddate = models.DateField()
     updatedate = models.DateField()
-
 # Create your models here.
 # comment
 # comment
