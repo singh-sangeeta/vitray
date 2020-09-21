14e2232c28f92278df7fe7b9e2b74fd62bb29189
singh-sangeeta <sangeetagehlot98@gmail.com>
2020-09-21 15:53:34 +0600

    create model

--- products/models.py
+++ products/models.py
@@ -4,23 +4,32 @@ class products(models.Model):
     product_id = models.AutoField()
     product_name = models.CharField(max_length=50)
     product_details = models.CharField(max_length=100)
-    createddate = models.DateField()
-    deleteddate = models.DateField()
-    updatedate = models.DateField()
-    categaries = models.ForeignKey()
+    categaries = models.ForeignKey(categaries,null=True,blank=True)
+    date_created = models.DateField()
+    date_deleted = models.DateField()
+    date_updated = models.DateField()
 
 class categaries(models.Model):
     id = models.AutoField(primary_Key=True)
     category = models.CharField(max_length=50)
+    date_created = models.DateField()
+    date_deleted = models.DateField()
+    date_updated = models.DateField()
 
 class productimage(models.Model):
     product_id = models.AutoField()
     productimage_id = models.AutoField()
-    productimage = models.ImageField()
+    product_image = models.ImageField(blank=True,null=True)
+    date_created = models.DateField()
+    date_deleted = models.DateField()
+    date_updated = models.DateField()
 
 class price(models.Model):
      product_id = models.AutoField()
      product_price = models.AutoField()
+
+
+
 # comment
 # comment
 # comment
