576562e8c81240f18e0b695f6321d45ac5fb1ea1
singh-sangeeta <sangeetagehlot98@gmail.com>
2020-09-25 14:40:44 +0600

    change in model

--- order/models.py
+++ order/models.py
@@ -1,38 +1,39 @@
 from django.db import models
-import users
-from products.models import products
-from django.db import transaction
-from django.core.validators import MinValueValidator, MaxValueValidator
-from datetime import datetime, timedelta
+from django.contrib.auth.models import User
+from products.models import Product
+from datetime import datetime
 
-class cart(models.model):
-    user_name = models.ForeignKey(users, null=True,blank=True)
-    product_name = models.ManyToManyField(products,blank=True)
-    total = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class cart(models.Model):
+    user_name = models.ForeignKey(User, null=False, blank=False, on_delete=models.SET(User))
+    price_total = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    product_name = models.ForeignKey(Product, null=False, blank=False, on_delete=models.SET(Product))
     date_created = models.DateField()
     date_deleted = models.DateField()
     date_updated = models.DateField()
 
+
 class Order(models.Model):
+    user = models.ForeignKey(User, null=False, blank=False,  on_delete=models.SET(User))
+    product_name = models.ForeignKey(Product, null=False, blank=False, on_delete=models.SET(Product))
+    status = models.CharField(max_length=20)
     email = models.EmailField()
 
-class Product(models.Model):
-    # ...
-    pass
 
-class OrderProduct(models.Model):
+class Order_Product(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
 
-class coupens(models.Model):
+class Coupens(models.Model):
     code = models.CharField(max_length=10, unique=True, blank=True)
-    valid_from = models.DateTimeField(default=datetime.now(), blank=True)
+    valid_from = models.DateTimeField(default=datetime.now(), blank=True, null=True)
     valid_to = models.DateTimeField(default=compute_default_to)
     discount = models.IntegerField(default=15, validators=[MinValueValidator(0), MaxValueValidator(100)])
-
     active = models.BooleanField(default=True)
+
 
 class Address(models.Model):
     street = models.TextField()
